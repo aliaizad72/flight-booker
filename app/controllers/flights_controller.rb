43961575d2358044816ac688d811e48d22109f1d
Ali Aizad <aliaizad72@gmail.com>
2024-08-30 19:02:04 +0800

    Create search results on view partials

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,6 +2,11 @@ class FlightsController < ApplicationController
   def index
     @airports = Airport.all.map(&:city)
     @dates = Flight.all.order(datetime: :asc).map { |flight| flight.datetime.strftime("%d-%m-%Y") }.uniq
+
+    @departure_id = Airport.find_by(city: params[:departure_airport])
+    @arrival_id = Airport.find_by(city: params[:arrival_airport])
+
+    @results = Flight.where(departure_id: @departure_id, arrival_id: @arrival_id)
   end
 
   private
