ffe44680c660e24bd2397e7cd5f5cb2db923fb2e
Ali Aizad <aliaizad72@gmail.com>
2024-08-30 16:23:05 +0800

    Add more seeds to Airports and Flights

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,6 +1,5 @@
 class FlightsController < ApplicationController
   def index
-    @departure_airports = Airport.all.select { |airport| airport.departing_flight.any? }.map(&:code)
-    @arrival_airports = Airport.all.select { |airport| airport.arriving_flight.any? }.map(&:code)
+    @airports = Airport.all.map(&:city)
   end
 end
