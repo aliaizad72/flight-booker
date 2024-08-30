4306c966ae722f1a895c89dd3ae957e0f28aa06c
Ali Aizad <aliaizad72@gmail.com>
2024-08-30 14:28:15 +0800

    Add flight seeds

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -1,6 +1,6 @@
 class Airport < ApplicationRecord
-  has_many :departing_flight, class_name: "Flight", foreign_key: :departure_id
-  has_many :arriving_flight, class_name: "Flight", foreign_key: :arrival_id
+  has_many :departing_flight, class_name: "Flight", foreign_key: :departure_id, dependent: :destroy
+  has_many :arriving_flight, class_name: "Flight", foreign_key: :arrival_id, dependent: :destroy
 
   validates :code, presence: true, length: { maximum: 3 }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,6 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
-Airport.destroy_all
-
 Airport.create!(
   [
     { code: "AMS" },
@@ -34,4 +32,31 @@ Airport.create!(
   ]
 )
 
-p "Created #{Airport.count} airports"
+puts "Created #{Airport.count} airports"
+
+Flight.create!(
+  [
+    { departure_id: 1, arrival_id: 2 },
+    { departure_id: 3, arrival_id: 4 },
+    { departure_id: 5, arrival_id: 6 },
+    { departure_id: 7, arrival_id: 8 },
+    { departure_id: 9, arrival_id: 10 },
+    { departure_id: 11, arrival_id: 12 },
+    { departure_id: 13, arrival_id: 14 },
+    { departure_id: 15, arrival_id: 16 },
+    { departure_id: 17, arrival_id: 18 },
+    { departure_id: 19, arrival_id: 20 },
+    { departure_id: 2, arrival_id: 3 },
+    { departure_id: 4, arrival_id: 5 },
+    { departure_id: 6, arrival_id: 7 },
+    { departure_id: 8, arrival_id: 9 },
+    { departure_id: 10, arrival_id: 11 },
+    { departure_id: 12, arrival_id: 13 },
+    { departure_id: 14, arrival_id: 15 },
+    { departure_id: 16, arrival_id: 17 },
+    { departure_id: 18, arrival_id: 19 },
+    { departure_id: 20, arrival_id: 1 }
+  ]
+)
+
+puts "Created #{Flight.count} flights"
